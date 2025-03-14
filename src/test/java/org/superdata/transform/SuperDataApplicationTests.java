61c6a7bb78651fa2e0335b9a172ccbc7bf97314c
guilhermevini2013 <guilhermevini2013@hotmail.com>
2025-03-14 20:58:01 -0300

    Setting project

---
+++ src/test/java/org/superdata/transform/SuperDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.superdata.transform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
