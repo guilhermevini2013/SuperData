13c98cb0f3bc505733aca9fc6620f0553e91c4ce
guilhermevini2013 <guilhermevini2013@hotmail.com>
2025-04-21 17:58:51 -0300

    initial commit

---
+++ src/main/java/org/superdata/SuperDataApplication.java
@@ -0,0 +1,13 @@
+package org.superdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/superdata/SuperDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.superdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
